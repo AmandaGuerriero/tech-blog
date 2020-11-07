4f75d673987a27eb9ba8b3c982eb7270cab3e06a
Amanda Guerriero <ag.codingclass@gmail.com>
2020-11-06 17:31:37 -0800

    Initial setup and instal

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// import models
+// const Post = require('./Post');
+
+// create associations
+// User.hasMany(Post, {
+//   foreignKey: 'user_id'
+// });
+
+
+// module.exports = { User, Post, Vote, Comment };
